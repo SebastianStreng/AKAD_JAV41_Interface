d159c71fa16474b0d3de84d720f8ba2ce4295d63
Sebastian Streng <51957686+SebastianStreng@users.noreply.github.com>
2022-10-17 10:36:00 +0200

    add interface and class

---
+++ src/Interface_Queue.java
@@ -0,0 +1,12 @@
+public interface Interface_Queue <E> {
+
+    public void enter( E elm);
+
+    public E leave ();
+
+    public boolean isEmpty();
+
+    public int size();
+
+    public void clear();
+}
---
+++ src/MyQueue.java
@@ -0,0 +1,26 @@
+public class MyQueue implements Interface_Queue{
+    @Override
+    public void enter(Object elm) {
+
+    }
+
+    @Override
+    public Object leave() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
